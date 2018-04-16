63544fb640e34bc6988b23393315b63bc816924a
fuzongjian <fu_zongjian@163.com>
2018-04-16 22:49:09 +0800

    修改。

---
+++ helloworld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by 付宗建 on 2018/4/15.
+//  Copyright © 2018年 youran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
